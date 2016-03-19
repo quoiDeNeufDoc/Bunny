beea3b2ee09c9cf09d3ca8e9aed7df3cefabe23a
Mathieu Cordebard <mathieu.cordebard@alcatel-lucent.com>
2016-03-19 16:53:23 +0100

    Commit user

--- server/services/userService.js
+++ server/services/userService.js
@@ -7,6 +7,33 @@ var jwt = httpService.jwt;
 
 console.log("[USER_SERVICE] ==== STARTING ====");
 
+
+app.get('/api/users/v1.0/users', function(req, res, next) {
+
+	var toto = [
+		{	
+			"phone" : "0689747249",
+   			"firstName" : "Dobromir",
+    		"lastName" : "Nikolov",
+    	},
+    	{	
+			"phone" : "0689747249",
+   			"firstName" : "Mathieu",
+    		"lastName" : "Cordebard",
+    	},
+    	{	
+			"phone" : "0689747249",
+   			"firstName" : "Cedric",
+    		"lastName" : "Simon",
+    	}
+
+    ]
+
+   
+		res.status(200).send(toto).end();
+
+});
+
 app.post('/api/users/v1.0/users/:phone', function (req, res, next) {
 	
 	// Get the phone parameter
